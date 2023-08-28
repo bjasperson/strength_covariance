fd8a997c0249f3f1d1aaec605dc91382a7d9f333
bjasperson <ben.jasperson@gmail.com>
2023-08-28 14:51:01 -0500

    explore: - added label_dict to cleanup plot labels - vac form energies added

--- strength_covariance/explore.py
+++ strength_covariance/explore.py
@@ -3,12 +3,24 @@ import numpy as np
 from model_selection import basic_outlier_removal
 import seaborn as sns
 import matplotlib.pyplot as plt
+import csv
 
 
-def pairplot_selected(df, factors, title):
+
+def import_label_dict():
+    df_labels = pd.read_csv("./strength_covariance/data_ays/label_dict.csv")
+    label_dict = df_labels.to_dict(orient="records")[0]
+    return label_dict
+
+
+def pairplot_selected(df, factors, title, label_dict):
     factors.extend(['strength_MPa', 'species'])
     X = df[factors]
+    X.columns = [label_dict[x] for x in X.columns.to_list()]
+    # X.columns = [x.replace("_"," ") for x in X.columns.to_list()]
     fig = sns.pairplot(X, hue='species')
+    # for ax in fig.axes.flatten():
+    #     ax.set_xlabel(ax.get_xlabel(), rotation=40, ha = "right")
     fig.savefig(f"./strength_covariance/data_ays/{title}.png", dpi=300)
     return
 
@@ -35,7 +47,7 @@ def save_corr_values(df, title):
     values.to_csv(f"./strength_covariance/data_ays/{title}.csv")
 
 
-def run_pairplots(df_clean):
+def run_pairplots(df_clean, label_dict):
 
     pairplot_selected(df_clean,
                       ['c11_fcc',
@@ -43,75 +55,87 @@ def run_pairplots(df_clean):
                        'c44_fcc',
                        'c11-c12-c44',
                        'c11-c12+c44'],
-                      'elastic_const_combos')
+                      'elastic_const_combos',
+                      label_dict)
 
     pairplot_selected(df_clean,
                       ['surface_energy_100_fcc',
                        'surface_energy_110_fcc',
                        'surface_energy_111_fcc',
                        'surface_energy_121_fcc'],
-                      'surface_energies')
+                      'surface_energies',
+                      label_dict)
 
     pairplot_selected(df_clean,
                       ['extr_stack_fault_energy_fcc',
                        'intr_stack_fault_energy_fcc',
                        'unstable_stack_energy_fcc',
                         'unstable_twinning_energy_fcc'],
-                      'stack_twin')
+                      'stack_twin',
+                      label_dict)
 
     pairplot_selected(df_clean,
                       ['lattice_constant_sc',
                        'lattice_constant_fcc',
                        'lattice_constant_bcc'],
-                      'lattice_consts')
+                      'lattice_consts',
+                      label_dict)
 
     pairplot_selected(df_clean,
                       ['cohesive_energy_bcc',
                        'cohesive_energy_sc',
                        'cohesive_energy_fcc'],
-                      'cohesive_energies')
+                      'cohesive_energies',
+                      label_dict)
 
     pairplot_selected(df_clean,
                       ['c44_bcc',
                        'c44_sc',
                        'c44_fcc',
                        'thermal_expansion_coeff_fcc'],
-                      'c44_thermal_exp')
+                      'c44_thermal_exp',
+                      label_dict)
 
     pairplot_selected(df_clean,
                       ['c11_bcc', 'c11_sc', 'c11_fcc'],
-                      'c11')
+                      'c11',
+                      label_dict)
 
     pairplot_selected(df_clean,
                       ['c12_bcc',
                        'c12_sc',
                        'c12_fcc'],
-                      'c12')
+                      'c12',
+                      label_dict)
 
     pairplot_selected(df_clean,
                       ['c44_sc',
                        'surface_energy_111_fcc',
                        'unstable_stack_energy_fcc'],
-                      'highly_correlated')
+                      'highly_correlated',
+                      label_dict)
 
     pairplot_selected(df_clean,
                       ['unstable_stack_energy_fcc',
                        'unstable_twinning_energy_fcc',
                        'unstable_twinning_energy_slip_fraction_fcc',
                        'unstable_stack_energy_slip_fraction_fcc'],
-                      'unstable_stack_twin')
+                      'unstable_stack_twin',
+                      label_dict)
 
     pairplot_selected(df_clean,
                       ['bulk_modulus_bcc',
                        'bulk_modulus_fcc',
                        'bulk_modulus_sc'],
-                      'bulk')
+                      'bulk',
+                      label_dict)
     
     pairplot_selected(df_clean,
                       ['c44_fcc',
                        'unstable_stack_energy_fcc',
                        'unstable_stack_energy_slip_fraction_fcc'],
-                      '230805')
+                      '230805',
+                      label_dict)
 
     pairplot_selected(df_clean,
                       ['intr_stack_fault_energy_fcc',
@@ -122,25 +146,29 @@ def run_pairplots(df_clean):
                        'lattice_constant_fcc',
                        'cohesive_energy_fcc',
                        'c44_fcc'],
-                      'big')
+                      'big',
+                      label_dict)
     
     pairplot_selected(df_clean,
                       ['relaxed_formation_potential_energy_fcc',
                        'unrelaxed_formation_potential_energy_fcc',
                        'vacancy_migration_energy_fcc',
                        'relaxation_volume_fcc'],
-                       'formation energies')
+                       'formation energies',
+                       label_dict)
     
     pairplot_selected(df_clean,
                       ['unstable_stack_energy_fcc',
                        'vacancy_migration_energy_fcc',
                        'c44_fcc',
                        'intr_stack_fault_energy_fcc'],
-                       'top4_w_formation')
+                       'top4_w_formation',
+                       label_dict)
 
 
 def main():
     # import data
+    label_dict = import_label_dict()
     df_in = pd.read_csv('./data/models_w_props.csv')
 
     save_corr_values(df_in, 'corr_initial')
@@ -166,7 +194,7 @@ def main():
         df_clean['c12_fcc'] + df_clean['c44_fcc']
 
     # uncomment to create pairplots
-    run_pairplots(df_clean)
+    run_pairplots(df_clean, label_dict)
     
 
     corr_plot_list = ['strength_MPa',
@@ -178,7 +206,9 @@ def main():
                       'lattice_constant_sc',
                       'lattice_constant_fcc',
                       'cohesive_energy_fcc',
-                      'c44_fcc']
+                      'c44_fcc',
+                      'vacancy_migration_energy_fcc',
+                      'relaxed_formation_potential_energy_fcc']
 
     correlation_plot(df_clean[corr_plot_list],
                      "corr_plot",
