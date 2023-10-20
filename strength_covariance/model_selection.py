c1feaa33c836cad425f9b34eb6ec6bd27af3bcd5
bjasperson <ben.jasperson@gmail.com>
2023-10-20 08:36:41 -0500

    added create_factor_select_plot and plotting - added loocv and kfold cv counts to factor select plot - clean up params_list (remove highly corr)

--- strength_covariance/model_selection.py
+++ strength_covariance/model_selection.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from itertools import combinations
 from sklearn.model_selection import train_test_split, RepeatedKFold, cross_val_score, LeaveOneOut, GridSearchCV
 from sklearn.impute import KNNImputer
@@ -11,6 +12,7 @@ from sklearn.decomposition import PCA
 from sklearn import linear_model, svm
 from ast import literal_eval
 import seaborn as sns
+import explore
 
 
 def basic_outlier_removal(df):
@@ -85,8 +87,9 @@ def factor_select_cv(X, y, pipe, n_factor_max=2, cv=5, scoring='r2'):
 
 def factor_percent_usage(df_results, N_lines, title):
     factor_list_in = df_results['factors'].iloc[:N_lines]
-    #factor_list = [literal_eval(i) for i in factor_list_in] #only needed when importing a csv
     factor_list = [i for i in factor_list_in]
+    if isinstance(factor_list[0], str):
+        factor_list = [literal_eval(i) for i in factor_list_in] #only needed when importing a csv
     factor_list_combined = [j for i in factor_list for j in i]
     df_factors = pd.DataFrame(factor_list_combined)
     fig = plt.figure()
@@ -94,11 +97,80 @@ def factor_percent_usage(df_results, N_lines, title):
     fig.add_axes(ax)
     fig.subplots_adjust(bottom=0.6)
     fig.savefig(f"./strength_covariance/model_ays/{title}.png", dpi=300)
-    return
+    return df_factors.value_counts().rename_axis('factor').reset_index(name=title)
 
+def create_factor_select_plot(df_merge, filename, label_dict):
+    
+    df_corr = df_merge.corr(numeric_only = True).round(2)
+    abs_strength_corr = abs(df_corr['strength_MPa']).sort_values(ascending=False)
+    abs_strength_corr.index.name = "factor"
+    abs_strength_corr = abs_strength_corr.rename('corr_coeff')
+
+    df_results_loocv = pd.read_csv("./strength_covariance/model_ays/loocv_models.csv", index_col=0)
+    df_results_kfold = pd.read_csv("./strength_covariance/model_ays/kfold_models.csv", index_col=0)
+    #need to rerun with num_factors to do this plot
+    #boxplot = df_results_loocv.boxplot("cv_score","num_factors")
+    #plt.savefig(f"./gb_covariance/model_ays/{filename}_boxplot.png", dpi=300)
+    df_factor_count_loocv = factor_percent_usage(df_results_loocv, 100, 'loocv_factor_count')
+    df_factor_count_kfold = factor_percent_usage(df_results_kfold, 100, 'kfold_factor_count')
+    
+    df = pd.merge(df_factor_count_loocv, abs_strength_corr, how="left", on='factor').set_index("factor").fillna(0)
+    df = pd.merge(df_factor_count_kfold, df, how="left", on='factor').set_index("factor").fillna(0)
+    #df = abs_coeff_corr.merge(df_factor_count, how="left", on='factor').set_index("factor").fillna(0)
+    df = df[['loocv_factor_count','kfold_factor_count','corr_coeff']]
+    df = (df - df.min())/(df.max() - df.min())
+    df = df.rename(columns = {"corr_coeff":"Correlation Coefficient",
+                              "loocv_factor_count":"Usage, LOOCV, top 100 models",
+                              "kfold_factor_count":"Usage, K-fold CV, top 100 models"})
+    
+    df = df.sort_values("Correlation Coefficient", ascending=False)
+    factor_select_plotting(df.iloc[:15,:], label_dict, filename+"_corr", width = 0.25)
+
+    df = df.sort_values("Usage, K-fold CV, top 100 models", ascending=False)
+    factor_select_plotting(df.iloc[:15,:], label_dict, filename+"_count", width = 0.25)
+
+
+def factor_select_plotting(df, label_dict, filename, width = 0.125): 
+    cols = df.columns
+
+    x = np.arange(len(df))    
+    multiplier = 0
+
+
+    plt.rc('xtick', labelsize=16) #fontsize of the x tick labels
+    plt.rc('ytick', labelsize=16) #fontsize of the y tick labels
+
+    fig, ax = plt.subplots(figsize=(16,10))
+    for attribute in cols:
+        offset = width * multiplier
+        scaled_value = df[attribute]
+        #scaled_value = (df[attribute]-min(df[attribute]))/(max(df[attribute])-min(df[attribute]))
+        #scaled_value[scaled_value==0.0] = 0.01 # set small value for plotting
+        ax.bar(x+offset,scaled_value,width, label=attribute)
+        #ax.set_ylabel("$R^2$ improvement (percentage)")
+
+        #axs[1].set_ylabel('Standardized Regression Coefficients (absolute)')
+        multiplier += 1
+    #ax.set_xticklabels(ax.get_xticklabels(), rotation = 90)
+    x_labels =  [label_dict[x] for x in df.index.to_list()]
+    ax.set_ylabel("Normalized Value\n(Value - Min Value)/(Max Value - Min Value)",fontsize=16)
+    tick_loc = (len(cols)*width/2-width/2)
+    ax.set_xticks(x + tick_loc, x_labels, rotation = 90)
+    ax.legend(bbox_to_anchor = (0,1,1,1), loc="lower center", mode="expand", ncol = 4)
+    ax.xaxis.set_minor_locator(ticker.FixedLocator(0.5+x+tick_loc))
+    ax.xaxis.grid(visible=True, which="minor")
+
+    #plt.show()
+    fig.tight_layout()
+    fig.savefig(f"./strength_covariance/model_ays/{filename}.png", dpi=300)
+
+
+    return
 
 def main():
     df_in = pd.read_csv("./data/models_w_props.csv")
+    label_dict = explore.import_label_dict()
+
 
     df_in = df_in.drop([i for i in df_in.columns if 'diamond' in i], axis=1)
     df_in = df_in.sample(frac=1)  # shuffle
@@ -108,10 +180,11 @@ def main():
                    'cohesive_energy_fcc', 'thermal_expansion_coeff_fcc',
                    'surface_energy_100_fcc',
                    'extr_stack_fault_energy',
-                   'intr_stack_fault_energy',
-                   'unstable_stack_energy', 'unstable_twinning_energy',
-                   'relaxed_formation_potential_energy_fcc',
-                   'unrelaxed_formation_potential_energy_fcc',
+                   # 'intr_stack_fault_energy', # highly correlated with extr SFE
+                   'unstable_stack_energy', 
+                   #'unstable_twinning_energy', #highly correlated with unstable SFE
+                   'relaxed_formation_potential_energy_fcc', #includes unrelaxed
+                   #'unrelaxed_formation_potential_energy_fcc',
                    'vacancy_migration_energy_fcc',
                    'relaxation_volume_fcc'
                    ]
@@ -138,19 +211,24 @@ def main():
 
     n_factor_max = 3
 
-    cv = RepeatedKFold(n_splits=10, n_repeats=3)
-    df_results = factor_select_cv(
-        X, y, pipe, n_factor_max=n_factor_max, cv=cv, scoring='neg_root_mean_squared_error')
-    df_results.to_csv("./strength_covariance/model_ays/kfold_models.csv")
-    # df_results = pd.read_csv("./strength_covariance/model_ays/kfold_models.csv")
-    factor_percent_usage(df_results, 100, 'kfold_factor_usage')
+    if False:
+        cv = RepeatedKFold(n_splits=10, n_repeats=3)
+        df_results = factor_select_cv(
+            X, y, pipe, n_factor_max=n_factor_max, cv=cv, scoring='neg_root_mean_squared_error')
+        df_results.to_csv("./strength_covariance/model_ays/kfold_models.csv")
+        # df_results = pd.read_csv("./strength_covariance/model_ays/kfold_models.csv")
+        factor_percent_usage(df_results, 100, 'kfold_factor_usage')
 
-    # loocv = LeaveOneOut()
-    # df_results_loocv = factor_select_cv(
-    #     X, y, pipe, n_factor_max=n_factor_max, cv=loocv, scoring='neg_root_mean_squared_error')
-    # df_results_loocv.to_csv("./strength_covariance/model_ays/loocv_models.csv")
-    # # df_results_loocv = pd.read_csv("./strength_covariance/model_ays/loocv_models.csv")
-    # factor_percent_usage(df_results_loocv, 100, 'loocv_factor_usage')
+    if False:
+        loocv = LeaveOneOut()
+        df_results_loocv = factor_select_cv(
+            X, y, pipe, n_factor_max=n_factor_max, cv=loocv, scoring='neg_root_mean_squared_error')
+        df_results_loocv.to_csv("./strength_covariance/model_ays/loocv_models.csv")
+        # df_results_loocv = pd.read_csv("./strength_covariance/model_ays/loocv_models.csv")
+        factor_percent_usage(df_results_loocv, 100, 'loocv_factor_usage')
+
+    if True:
+        create_factor_select_plot(df_in, "factor_importance", label_dict)
     return
 
 
